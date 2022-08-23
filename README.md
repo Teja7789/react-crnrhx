4e9c67fb9061095772eaf7e319a8405bb448b3c6
Teja7789 <rx100789@gmail.com>
2022-08-23 18:45:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-crnrhx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-crnrhx)
---
+++ src/Pages/Login/index.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Navigation from "../../assets/images/Navigation.png";
+import openeye from "../../assets/images/openeye.png";
+import { useHistory } from "react-router";
+
+function App() {
+  const history = useHistory();
+  return (
+    <div className="App">
+      <div className="login-page">
+        <div className="main-content">
+          <div className="company-logo">
+            <img src={Navigation} alt="navigation-logo" />
+          </div>
+          <div className="form-content">
+            <h4 role="heading" aria-level="1">
+              Welcome!
+            </h4>
+            <p>Please login using your account</p>
+            <form>
+              <div className="form-group login-fields">
+                <label>USERNAME</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value="demo@pdsgalaxy.com"
+                  aria-label="Username"
+                />
+              </div>
+              <div className="form-group login-fields">
+                <label>PASSWORD</label>
+                <input type="password" className="form-control" placeholder="" value="test@123" />
+                <img src={openeye} alt="openeye-logo" className="password-img" />
+              </div>
+              <button
+                type="button"
+                onClick={() => history.push("/account-details")}
+                // eslint-disable-next-line react/no-unknown-property
+                class="login-btn btn-block"
+              >
+                LOGIN
+              </button>
+              <div className="more-info">
+                <div className="forgot-pw">
+                  <a href="/">Forgot your password?</a>
+                </div>
+                <div className="reset">{/* <a href="/"></a> */}</div>
+              </div>
+              <button type="submit" class="signup-btn btn-block">
+                CREATE AN ACCOUNT
+              </button>
+            </form>
+          </div>
+        </div>
+        <div class="copyright">
+          <p style={{ color: "#000" }}>&copy;2021 PDS Galaxy</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
